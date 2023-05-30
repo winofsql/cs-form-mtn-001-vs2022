64ffc6954e76bcbec3c976d4f5181e938e8f57a1
sworc <lightbox@nifty.com>
2023-05-30 11:24:17 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ƒeƒXƒgToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            {
+                //Form2 dialog = new Form2();
+                //DialogResult ret = dialog.ShowDialog(this);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+    }
+}
